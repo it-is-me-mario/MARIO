74fab184ff51c3df70b1322c63d82a22fd255815
Mohammad Amin Tahavori <50955527+mohammadamint@users.noreply.github.com>
2022-03-23 21:24:19 +0100

    iomath unittest
    - Testing all the equations for the IOT matrices case

--- mario/test/mario_test.py
+++ mario/test/mario_test.py
@@ -12,13 +12,20 @@ import pandas as pd
 _DATA_MAP = {
     'X': dict(sheet_name='X',index_col=[0,1,2],header=0),
     'Y': dict(sheet_name='Y',index_col=[0,1,2],header=[0,1,2]),
+    'y': dict(sheet_name='_y',index_col=[0,1,2],header=[0,1,2]),
     'E': dict(sheet_name='E',index_col=[0],header=[0,1,2]),
     'e': dict(sheet_name='_e',index_col=[0],header=[0,1,2]),
     'V': dict(sheet_name='V',index_col=[0],header=[0,1,2]),
     'v': dict(sheet_name='_v',index_col=[0],header=[0,1,2]),
     'Z': dict(sheet_name='Z',index_col=[0,1,2],header=[0,1,2]),
     'z': dict(sheet_name='_z',index_col=[0,1,2],header=[0,1,2]),
+    'b': dict(sheet_name='b',index_col=[0,1,2],header=[0,1,2]),
+    'g': dict(sheet_name='g',index_col=[0,1,2],header=[0,1,2]),
     'w': dict(sheet_name='w',index_col=[0,1,2],header=[0,1,2]),
+    'f': dict(sheet_name='_f',index_col=[0],header=[0,1,2]),
+    'F': dict(sheet_name='F',index_col=[0],header=[0,1,2]),
+    'm': dict(sheet_name='_m',index_col=[0],header=[0,1,2]),
+    'M': dict(sheet_name='M',index_col=[0],header=[0,1,2]),
     'p': dict(sheet_name='p',index_col=[0,1,2],header=[0]),
 }
 
--- tests/test_iomath.py
+++ tests/test_iomath.py
@@ -1,7 +1,6 @@
 """Mathematical Engine Tests"""
 
-#%%import pandas as pd
-import numpy as np
+
 import pytest
 import pandas.testing as pdt
 
@@ -31,7 +30,6 @@ from mario.tools.iomath import (
     calc_b,
     calc_F,
     calc_f,
-    calc_f_dis,
     calc_m,
     calc_M,
     calc_y,
@@ -54,6 +52,11 @@ def test_calc_X_from_z(IOT_table):
 def test_calc_X_from_w(IOT_table):
     pdt.assert_frame_equal(
         IOT_table['X'],calc_X_from_w(IOT_table['w'],IOT_table['Y'])
+    )
+
+def test_calc_X(IOT_table):
+    pdt.assert_frame_equal(
+        IOT_table['X'],calc_X(IOT_table['Z'],IOT_table['Y'])
     )
 
 def test_calc_w(IOT_table):
@@ -95,3 +98,41 @@ def test_calc_p(IOT_table):
     pdt.assert_frame_equal(
         IOT_table['p'],calc_p(IOT_table['v'],IOT_table['w'])
     )
+
+def test_calc_y(IOT_table):
+    pdt.assert_frame_equal(
+        IOT_table['y'],calc_y(IOT_table['Y'])
+    )
+
+    assert calc_y(IOT_table['y']).sum().sum() == 1
+
+def test_calc_b(IOT_table):
+    pdt.assert_frame_equal(
+        IOT_table['b'],calc_b(IOT_table['X'],IOT_table['Z'])
+    )
+
+def test_calc_g(IOT_table):
+    pdt.assert_frame_equal(
+        IOT_table['g'],calc_g(IOT_table['b'])
+    )
+
+def test_calc_f(IOT_table):
+    pdt.assert_frame_equal(
+        IOT_table['f'],calc_f(IOT_table['e'],IOT_table['w'])
+    )
+
+def test_calc_F(IOT_table):
+    pdt.assert_frame_equal(
+        IOT_table['F'],calc_F(IOT_table['f'],IOT_table['Y'])
+    )
+
+def test_calc_m(IOT_table):
+    pdt.assert_frame_equal(
+        IOT_table['m'],calc_m(IOT_table['v'],IOT_table['w'])
+    )
+
+def test_calcM(IOT_table):
+    pdt.assert_frame_equal(
+        IOT_table['M'],calc_M(IOT_table['m'],IOT_table['Y'])
+    )
+
