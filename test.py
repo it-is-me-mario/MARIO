60b2445ba5ed1a44071ca4069175be1e57875f8d
Lorenzo Rinaldi <lorenzo.rinaldi13@gmail.com>
2022-12-21 13:57:50 +0100

    Function parse_exiobase_sut has been updated to parse exiobase 3.3.18 (hybrid) version available on zenodo repository

---
+++ test.py
@@ -0,0 +1,12 @@
+#%%
+import mario
+
+#%%
+world = mario.parse_exiobase_sut(
+    path=r"C:\Users\loren\Politecnico di Milano\DENG-SESAM - Documenti\DATASETS\Exiobase Hybrid 3.3.18", 
+    units="hybrid", 
+    version="3.3.18", 
+    extensions='all'
+    )
+
+# %%
