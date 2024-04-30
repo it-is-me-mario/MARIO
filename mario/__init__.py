c2718cf4d8183be012dc03ef7a4d357752a0d5fe
Mohammad Amin Tahavori <50955527+mohammadamint@users.noreply.github.com>
2024-05-01 01:09:04 +0200

    add IOT and SUT to the constans and __init__

--- mario/__init__.py
+++ mario/__init__.py
@@ -91,6 +91,6 @@ from mario.test.mario_test import load_test
 from mario.tools.utilities import slicer
 from mario.tools.iodownloader import *
 from mario.settings.settings import upload_settings,download_settings,reset_settings,Nomenclature,Index
-
+from mario.tools.constants import IOT,SUT
 
 __authors__ = " 'Mohammad Amin Tahavori', Lorenzo Rinaldi', 'Nicolò Golinucci' "
